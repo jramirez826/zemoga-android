9617922d72171e0f89f6f4a110ff1bc5d6eed4c9
carlos pava <carpavaroty@gmail.com>
2021-05-25 15:46:50 -0500

    added tabs to all and favorite posts

--- app/src/main/java/com/jramirez/pruebazemoga/presentation/posts/allposts/AllPostsViewModel.kt
+++ app/src/main/java/com/jramirez/pruebazemoga/presentation/posts/allposts/AllPostsViewModel.kt
@@ -1,7 +1,10 @@
 package com.jramirez.pruebazemoga.presentation.posts.allposts
 
 import android.app.Application
-import androidx.lifecycle.*
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.jramirez.pruebazemoga.domain.entity.UIPost
 import com.jramirez.pruebazemoga.domain.interactor.PostInteractor
 import kotlinx.coroutines.launch
@@ -11,7 +14,7 @@ class AllPostsViewModel(
 ) : AndroidViewModel(application) {
 
     private val postInteractor: PostInteractor = PostInteractor(application.applicationContext)
-
+    private var posts: List<UIPost> = emptyList()
     private val _livePosts = MutableLiveData<List<UIPost>>()
     val livePosts: LiveData<List<UIPost>> get() = _livePosts
 
@@ -19,9 +22,21 @@ class AllPostsViewModel(
         viewModelScope.launch {
             val response = postInteractor.execute()
             response.data?.let {
+                posts = it
                 _livePosts.postValue(it)
             }
         }
+    }
+
+    fun allPostsTab() {
+        _livePosts.postValue(posts)
+    }
+
+    fun favoritePostsTab() {
+        val favoritePosts = posts.filter {
+            it.isFavorite
+        }
+        _livePosts.postValue(favoritePosts)
     }
 
 }
--- app/src/main/java/com/jramirez/pruebazemoga/presentation/posts/allposts/PostsActivity.kt
+++ app/src/main/java/com/jramirez/pruebazemoga/presentation/posts/allposts/PostsActivity.kt
@@ -1,4 +1,4 @@
-package com.jramirez.pruebazemoga.presentation.posts
+package com.jramirez.pruebazemoga.presentation.posts.allposts
 
 import android.os.Bundle
 import android.view.Menu
@@ -7,22 +7,23 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
+import com.google.android.material.tabs.TabLayout
 import com.jramirez.pruebazemoga.R
 import com.jramirez.pruebazemoga.databinding.ActivityPostsBinding
 import com.jramirez.pruebazemoga.domain.entity.UIPost
 import com.jramirez.pruebazemoga.presentation.application.ZemogaApplication
-import com.jramirez.pruebazemoga.presentation.posts.allposts.AllPostsAdapter
-import com.jramirez.pruebazemoga.presentation.posts.allposts.AllPostsViewModel
-import com.jramirez.pruebazemoga.presentation.posts.allposts.AllPostsViewModelFactory
 import com.jramirez.pruebazemoga.presentation.util.CellClickListener
 import com.jramirez.pruebazemoga.presentation.util.DeleteItemCallback
 
-class PostsActivity : AppCompatActivity(), CellClickListener<UIPost> {
+class PostsActivity : AppCompatActivity(), CellClickListener<UIPost>,
+    TabLayout.OnTabSelectedListener {
 
     private lateinit var binding: ActivityPostsBinding
     private val adapter: AllPostsAdapter = AllPostsAdapter(this)
     private val deleteItemCallback: DeleteItemCallback = DeleteItemCallback(adapter)
     private lateinit var allPostsViewModel: AllPostsViewModel
+    private lateinit var tabAll: TabLayout.Tab
+    private lateinit var tabFavorite: TabLayout.Tab
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,6 +41,11 @@ class PostsActivity : AppCompatActivity(), CellClickListener<UIPost> {
     private fun setUpBinding() {
         with(binding) {
             setSupportActionBar(toolbar)
+            tabAll = tabs.newTab().setId(1).setText(R.string.tab_all_post)
+            tabFavorite = tabs.newTab().setId(2).setText(R.string.tab_favorite_post)
+            tabs.addTab(tabAll)
+            tabs.addTab(tabFavorite)
+            tabs.addOnTabSelectedListener(this@PostsActivity)
             rvPosts.adapter = adapter
             binding.rvPosts.adapter = adapter
             val itemTouchHelper = ItemTouchHelper(deleteItemCallback)
@@ -70,5 +76,19 @@ class PostsActivity : AppCompatActivity(), CellClickListener<UIPost> {
             R.id.menu_refresh -> allPostsViewModel.getPosts()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onTabSelected(tab: TabLayout.Tab?) {
+        when (tab?.id) {
+            tabAll.id -> allPostsViewModel.allPostsTab()
+            tabFavorite.id -> allPostsViewModel.favoritePostsTab()
+        }
+    }
+
+    override fun onTabUnselected(tab: TabLayout.Tab?) {
+    }
+
+    override fun onTabReselected(tab: TabLayout.Tab?) {
+        onTabSelected(tab)
     }
 }
